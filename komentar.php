074a041273c94406990089644854ba4fc6e7643f
Angelina Yona Listianingsih <43510088+angelinayl@users.noreply.github.com>
2018-10-05 10:23:02 +0700

    Komentar
    Berisi form komentar

--- komentar.php
+++ komentar.php
@@ -1,3 +1,28 @@
-<?php
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td>:</td>
+			<td><input type="number" name="nim" length="10"></td>
+		</tr>
 
-?>
+		
+		<tr>
+			<td><input type="submit" name="kirim"></td>
+		</tr>		
+	</table>
+</form>
+
+<?php 
+if(isset($_POST['kirim'])){
+	$komentar =$_POST['komentar'];
+
+	if(str_word_count($komentar)<5){
+	echo "Error, NIM Maksimal 10 Karakter";
+	}else{
+		echo str_word_count($komentar);
+
+	}
+}
+	
+	?>
